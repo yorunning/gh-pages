55e3b2d6ac9b34bc4713cc1742517b15d97d2b70
Yorun <547747006@qq.com>
2022-11-18 19:03:24 +0800

    chore(memo): update sidebar

--- memo/src/.vuepress/sidebar.ts
+++ memo/src/.vuepress/sidebar.ts
@@ -1,9 +1,25 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const Sidebar = sidebar({
+  "/": [
+    {
+      text: "ArchLinux安装",
+      icon: "linux",
+      prefix: "archlinux/",
+      children: [
+        "preinstallation",
+        "basic-installation",
+        "desktop-environment-installation",
+        "use-configuration",
+        "dual-system",
+      ],
+    },
+  ],
+
   "/archlinux/": [
     {
       text: "ArchLinux安装",
+      icon: "linux",
       children: [
         "preinstallation",
         "basic-installation",
